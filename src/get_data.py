7ad31d36d48799269d884ec98b609cb4cd2b93b4
Nishvanth <nishvantht@gmail.com>
2024-05-19 12:40:18 +0530

    stage 1 completed

--- src/get_data.py
+++ src/get_data.py
@@ -6,7 +6,7 @@ import argparse
 def read_params(config_path): 
     with open(config_path) as yaml_file: 
         config = yaml.safe_load(yaml_file)
-    return  config
+    return config
 
 def get_data(config_path):
     config = read_params(config_path)
